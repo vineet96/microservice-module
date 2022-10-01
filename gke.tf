a095cab00839bf8aead52de3856ab0ba15886c4c
Vineet Agarwal <vineetagarwal@google.com>
2022-10-01 12:00:33 -0700

    updated number of nodes

--- gke.tf
+++ gke.tf
@@ -7,7 +7,7 @@ resource "google_container_cluster" "primary" {
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   remove_default_node_pool = true
-  initial_node_count       = 1
+  initial_node_count       = 2
   enable_shielded_nodes = true
 
   network    = var.vpc
